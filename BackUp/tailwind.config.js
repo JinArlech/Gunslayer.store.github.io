31b1c773b61ed8d023047b524e62a29200d2dcbe
JinArlech <arlecchinojin@gmail.com>
2024-01-28 14:25:16 +0700

    Gunslayer
    Just the store i create

---
+++ BackUp/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'Inter': ['Inter'],
+      },
+      colors: {
+        clifford: '#da373d',
+      },
+      screens: {
+        'HD': '1080px',
+        '2k': '2048px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
